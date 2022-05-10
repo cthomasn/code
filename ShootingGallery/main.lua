00ba9d215fb3d511cf0ef87cfad63a86ce4fc15d
cthomasn <104948196+cthomasn@users.noreply.github.com>
2022-05-10 14:35:33 -0400

    More LOVE2D learning

---
+++ ShootingGallery/main.lua
@@ -0,0 +1,94 @@
+--[[
+    marchaelj
+    2022JAN12
+    THREE MAIN FUNCTIONS TO MAKE A LOVE2D GAME WORK:
+    ** LOVE.LOAD(), LOVE.UPDATE(DT), AND LOVE.DRAW() **
+]]
+
+
+
+function love.load()
+
+   target = {}
+   target.x = 300
+   target.y = 300
+   target.radius = 50
+
+   score = 0
+   timer = 0
+   gameState = 1
+
+   gameFont = love.graphics.newFont(40)
+
+   sprites = {}
+   sprites.sky = love.graphics.newImage('Sprites/sky.png')
+   sprites.target = love.graphics.newImage('Sprites/target.png')
+   sprites.crosshairs = love.graphics.newImage('Sprites/crosshairs.png')
+
+    love.mouse.setVisible(false)
+end
+
+function love.update(dt)
+
+    -- game loop code
+    if timer > 0 then
+        timer = timer - dt
+    end
+    
+    if timer < 0 then
+        timer = 0
+        gameState = 1
+        score = 0
+    end
+
+end
+
+
+function love.draw()
+
+    -- graphics / images
+    -- love.graphics.rectangle("fill", 200, 250, 200, 100)
+    -- -- the code below affects the circle below
+    -- love.graphics.setColor(1, 0.63, 0)
+    -- love.graphics.circle("fill", 300, 200, 100)
+    -- -- the code below cycles through the code in the function
+    -- -- since there is no shape directly underneath it
+    -- -- therefore it affects the rectangle that was drawn first
+    -- love.graphics.setColor(0, 1, 0)
+
+    love.graphics.draw(sprites.sky, 0, 0)
+
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.setFont(gameFont)
+    love.graphics.print('Score: ' .. score, 5, 5)
+    love.graphics.print('Timer: ' .. math.ceil(timer), 300, 5)
+
+    if gameState == 1 then
+        love.graphics.printf('Click anywhere to begin', 0, 250, love.graphics.getWidth(), "center")
+    end
+
+    if gameState == 2 then
+        love.graphics.draw(sprites.target, target.x - target.radius, target.y - target.radius)
+    end
+
+    love.graphics.draw(sprites.crosshairs, love.mouse.getX() - 20, love.mouse.getY() - 20)
+
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+    if button == 1 and gameState == 2 then
+        local mouseToTarget = distanceBetween(x, y, target.x, target.y)
+        if mouseToTarget < target.radius then
+            score = score + 1
+            target.x = math.random(target.radius, love.graphics.getWidth() - target.radius)
+            target.y = math.random(100, love.graphics.getHeight() - target.radius)
+        end
+    elseif button == 1 and gameState == 1 then
+        gameState = 2
+        timer = 10
+    end
+end
+
+function distanceBetween(x1, y1, x2, y2)
+    return math.sqrt( (x2 - x1)^2 + (y2 - y1)^2 )
+end
